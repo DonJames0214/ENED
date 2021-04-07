3d944da1c605cd1b0325b58f3bc897ae05fca705
DonJames0214 <donmcfarland74@yahoo.com>
2021-04-07 03:16:02 -0400

    v0.4

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="ENED_ToolKit",
-      author="DJ McFarland",
-      version="0.4",
-      py_packager= ['ENED_ToolKit'])
+#setup(name="ENED_ToolKit",
+#      author="DJ McFarland",
+#      version="0.4",
+#      py_packager= ['ENED_ToolKit'])
+
+setup()
